9e6f6e58fa477e9eea9845e0dcb5fc67abdadd0b
KillianVendewinkele <killian.vendewinkele@gmail.com>
2024-01-24 09:43:40 +0100

    add mqtt

--- socket/server.js
+++ socket/server.js
@@ -1,11 +1,14 @@
 var SerialPort = require('serialport');
 var xbee_api = require('xbee-api');
 var C = xbee_api.constants;
+var h2o_rate = 0;
 //var storage = require("./storage")
 require('dotenv').config()
 
 
 const SERIAL_PORT = process.env.SERIAL_PORT;
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://test.mosquitto.org");
 
 var xbeeAPI = new xbee_api.XBeeAPI({
   api_mode: 2
@@ -25,15 +28,7 @@ xbeeAPI.builder.pipe(serialport);
 serialport.on("open", function () {
   // frame_obj = { // AT Request to be sent
   //   type: C.FRAME_TYPE.AT_COMMAND,
-  //   command: "DH",
-  //   commandParameter: [],
-  // };
-
-  // xbeeAPI.builder.write(frame_obj);
-
-  // frame_obj = { // AT Request to be sent
-  //   type: C.FRAME_TYPE.AT_COMMAND,
-  //   command: "DL",
+  //   command: "D0",
   //   commandParameter: [],
   // };
 
@@ -53,37 +48,58 @@ serialport.on("open", function () {
 // All frames parsed by the XBee will be emitted here
 
 // storage.listSensors().then((sensors) => sensors.forEach((sensor) => console.log(sensor.data())))
+client.on("connect", () => {
+  xbeeAPI.parser.on("data", function (frame) {
 
-xbeeAPI.parser.on("data", function (frame) {
+    //on new device is joined, register it
+    // console.log(C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET);
+    //on packet received, dispatch event
+    // let dataReceived = String.fromCharCode.apply(null, frame.data);
+    if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
+      console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
+      let dataReceived = String.fromCharCode.apply(null, frame.data);
+      console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
+  
+    }
+  
+    if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
+      // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
+      console.log("NODE_IDENTIFICATION");
+      //storage.registerSensor(frame.remote64)
+  
+    } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
+      h2o_rate = frame.analogSamples.AD2*100/1024;
+      if(h2o_rate <= 20) {
+        client.subscribe("Humidity rate", (err) => {
+          if (!err) {
+            client.publish("Humidity rate", "The humidity rate of the plant is low (" + h2o_rate + "%)");
+          }
+        });
+      }
+      module.exports = h2o_rate;
 
-  //on new device is joined, register it
-  // console.log(C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET);
-  //on packet received, dispatch event
-  // let dataReceived = String.fromCharCode.apply(null, frame.data);
-  if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
-    console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
-    let dataReceived = String.fromCharCode.apply(null, frame.data);
-    console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
+      // frame_obj = {
+      //   type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+      //   destination64: "0030A2003R2F24F5",
+      //   command: "D0",
+      //   commandParameter: [0o4],
+      // };
 
-  }
+      // xbeeAPI.builder.write(frame_obj);
 
-  if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
-    // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
-    console.log("NODE_IDENTIFICATION");
-    //storage.registerSensor(frame.remote64)
+    } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
+      console.log("REMOTE_COMMAND_RESPONSE")
+    } else {
+      console.debug(frame);
+      let dataReceived = String.fromCharCode.apply(null, frame.commandData)
+      console.log(dataReceived);
+    }
+  
+  });
+});
 
-  } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
-
-    console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
-    console.log(frame.analogSamples.AD2)
-    //storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
-
-  } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
-    console.log("REMOTE_COMMAND_RESPONSE")
-  } else {
-    console.debug(frame);
-    let dataReceived = String.fromCharCode.apply(null, frame.commandData)
-    console.log(dataReceived);
-  }
-
+client.on("message", (topic, message) => {
+  // message is Buffer
+  console.log(topic);
+  console.log(message.toString());
 });
